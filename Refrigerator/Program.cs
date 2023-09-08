d2e55d651697f3730603fd0bc98eb51ae37d7246
Roma Rathi <Roma.Rathi+EYGS@ey.com>
2023-09-08 12:00:47 +0530

    refrigerator app

---
+++ Refrigerator/Program.cs
@@ -0,0 +1,109 @@
+﻿using PAthlock;
+
+
+class Program
+{
+    static void Main()
+    {
+        var refrigerator = new Refrigerator();
+
+        while (true)
+        {
+            Console.WriteLine("Refrigerator App");
+            Console.WriteLine("1. Insert Product");
+            Console.WriteLine("2. Consume Product");
+            Console.WriteLine("3. Current Status");
+            Console.WriteLine("4. Check Expiry");
+            Console.WriteLine("5. Exit");
+            Console.Write("Enter your choice: ");
+
+            if (int.TryParse(Console.ReadLine(), out int choice))
+            {
+                switch (choice)
+                {
+                    case 1:
+                        InsertProduct(refrigerator);
+                        break;
+                    case 2:
+                        ConsumeProduct(refrigerator);
+                        break;
+                    case 3:
+                        DisplayCurrentStatus(refrigerator);
+                        break;
+                    case 4:
+                        CheckExpiry(refrigerator);
+                        break;
+                    case 5:
+                        Environment.Exit(0);
+                        break;
+                    default:
+                        Console.WriteLine("Invalid choice. Please try again.");
+                        break;
+                }
+            }
+            else
+            {
+                Console.WriteLine("Invalid input. Please enter a number.");
+            }
+        }
+    }
+
+    static void InsertProduct(Refrigerator refrigerator)
+    {
+        Console.Write("Enter product name: ");
+        string productName = Console.ReadLine();
+        Console.Write("Enter quantity: ");
+        if (int.TryParse(Console.ReadLine(), out int quantity))
+        {
+            Console.Write("Enter expiration date (yyyy-MM-dd): ");
+            if (DateTime.TryParse(Console.ReadLine(), out DateTime expiryDate))
+            {
+                refrigerator.InsertProduct(productName, quantity, expiryDate);
+                Console.WriteLine("Product inserted successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Invalid date format. Please use yyyy-MM-dd.");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Invalid quantity. Please enter a number.");
+        }
+    }
+
+    static void ConsumeProduct(Refrigerator refrigerator)
+    {
+        Console.Write("Enter product name: ");
+        string productName = Console.ReadLine();
+        Console.Write("Enter quantity consumed: ");
+        if (int.TryParse(Console.ReadLine(), out int quantity))
+        {
+            refrigerator.ConsumeProduct(productName, quantity);
+            Console.WriteLine("Product consumed successfully.");
+        }
+        else
+        {
+            Console.WriteLine("Invalid quantity. Please enter a number.");
+        }
+    }
+
+    static void DisplayCurrentStatus(Refrigerator refrigerator)
+    {
+        var currentStatus = refrigerator.GetCurrentStatus();
+        Console.WriteLine("Current Status:");
+        foreach (var item in currentStatus)
+        {
+            Console.WriteLine($"{item.Key}: {item.Value} (Expires on: {item.Key.ExpiryDate:yyyy-MM-dd})");
+        }
+    }
+
+    static void CheckExpiry(Refrigerator refrigerator)
+    {
+        var expiredProducts = refrigerator.CheckExpiry();
+        foreach (var product in expiredProducts)
+        {
+            Console.WriteLine($"Remove {product.Name} from the refrigerator. It has expired.");
+        }
+    }
+}
---
+++ Refrigerator/Refrigerator.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAthlock
+{
+    class Refrigerator
+    {
+        private readonly Dictionary<Product, int> products = new Dictionary<Product, int>();
+
+        public void InsertProduct(string productName, int quantity, DateTime expiryDate)
+        {
+            var product = new Product { Name = productName, Quantity = quantity, ExpiryDate = expiryDate };
+            if (products.ContainsKey(product))
+            {
+                products[product] += quantity;
+            }
+            else
+            {
+                products.Add(product, quantity);
+            }
+        }
+
+        public void ConsumeProduct(string productName, int quantity)
+        {
+            var product = products.Keys.FirstOrDefault(p => p.Name.Equals(productName, StringComparison.OrdinalIgnoreCase));
+            if (product != null && products[product] >= quantity)
+            {
+                products[product] -= quantity;
+                if (products[product] == 0)
+                {
+                    products.Remove(product);
+                }
+            }
+            else
+            {
+                Console.WriteLine($"Not enough {productName} in the refrigerator.");
+            }
+        }
+
+        public Dictionary<Product, int> GetCurrentStatus()
+        {
+            return products;
+        }
+
+        public List<Product> CheckExpiry()
+        {
+            var currentDate = DateTime.Now;
+            var expiredProducts = products.Keys.Where(p => p.ExpiryDate < currentDate).ToList();
+            foreach (var product in expiredProducts)
+            {
+                products.Remove(product);
+            }
+            return expiredProducts;
+        }
+    }
+}
